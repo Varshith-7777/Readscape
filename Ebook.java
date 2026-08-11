28b002ce439f2d77d19c417b290e91ea01a6bab4
VARSHITH DHARIPALLY <25211a05kq@bvrit.ac.in>
2026-08-11 12:15:22 +0530

    Add Ebook class extending Book and implementing Downloadable

---
+++ Ebook.java
@@ -0,0 +1,41 @@
+public class Ebook extends Book implements Downloadable
+{
+    private float fileSize;
+    private int pageNums;
+
+    Ebook(String title, String author, float price, float fileSize,int pageNums)
+    {
+        super(title, author, price);
+        this.fileSize = fileSize;
+        this.pageNums = pageNums;
+    }
+
+    Ebook()
+    {
+    }
+
+    void setFilesize(float fileSize)
+    {
+        this.fileSize = fileSize;
+    }
+
+    float getFilesize()
+    {
+        return fileSize;
+    }
+
+    @Override
+    public void display()
+    {
+        super.display();
+        System.out.println("File Size : " +fileSize + " KB");
+        System.out.println("Page No : " + pageNums );
+
+    }
+
+    @Override
+    public void download()
+    {
+        System.out.println(getTitle() + " PDF book is downloading...");
+    }
+}
